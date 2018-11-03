084b8db4ffdf64a28ecbffa0d5f6a550d9e6070a
unknown <kathiavegamina@live.com.mx>
2018-11-03 14:35:38 -0600

    prueba a develop
    solo probar

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import UsersMenu from './components/Home/Users/UsersMenu';
 import DependantsMenu from './components/Home/Dependants/DependantsMenu';
 import './App.scss';
 import NavBar from './NavBar';
-import { Paper, AppBar } from '@material-ui/core/';
+import  AppBar from '@material-ui/core/AppBar';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <AppBar><NavBar></NavBar></AppBar>
+          <AppBar><NavBar></NavBar></AppBar>      
           <Switch>
             <Route
               path="/Home"
