d6ed6e019fd7653aa76606cef42fbbe5c2e88a0f
unknown <kathiavegamina@live.com.mx>
2018-11-08 22:47:41 -0600

    modificaciones

--- src/components/Home/Users/ListaUsers.js
+++ src/components/Home/Users/ListaUsers.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import {  Button, AppBar, Toolbar, Typography, Paper, Table, TableBody, TableCell, TableHead, TableRow, } from '@material-ui/core/Button';
+import {  Button, AppBar, Toolbar, Typography, Paper } from '@material-ui/core/Button';
 import { connect } from 'react-redux';
 import { fetchUsers } from './../../../state/actions/Users/UsersActions/UsersActions';
 import  { Link }  from 'react-router-dom';
 import NavBar from '../../../NavBar';
-import PropTypes from 'prop-types';
+import TableUser from './TableUser';
 
 class ListaUsers extends Component{
 
-handleClickGetUsers = () => {
-    this.props.fetchUsers();
-}
+// handleClickGetUsers = () => {
+//     this.props.fetchUsers();
+// }
 
     render () {
         return (
             <div className='lista_user'>
-                <AppBar  position="static" color="success"> 
+                <AppBar  position="static" color="secondary"> 
                     <NavBar /> 
                     <Link to="/Home" className="link"><Button>Home</Button></Link>    
                     <Toolbar>
@@ -25,35 +25,11 @@ handleClickGetUsers = () => {
                     </Toolbar>  
                 </AppBar> 
                 <Paper> 
-                    <Button onClick={this.handleClickGetUsers}>Cargar userss</Button>                  
+                    {/* <Button onClick={this.handleClickGetUsers}>Cargar userss</Button>                   */}
                     
-                        <Paper>    
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                    <TableCell>Nombre</TableCell>
-                                    <TableCell>Apellido Paterno</TableCell>
-                                    <TableCell>Apellido Materno</TableCell>
-                                    <TableCell>Edad</TableCell>
-                                </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {this.props.users.users.map(users => {
-                                        return (
-                                            <TableRow key={users}>
-                                                <TableCell component="th" scope="users" >{users.nombre}</TableCell>
-                                                <TableCell component="th" scope="users" >{users.apellidos.paterno}</TableCell>
-                                                <TableCell component="th" scope="users" >{users.apellidos.materno}</TableCell>
-                                                <TableCell numeric="th" scope="users" >{users.edad}</TableCell>
-                                            </TableRow>
-                                        )
-                                    })}
-                                </TableBody>
-                            </Table>
-                        </Paper>  
-                    
-                                
-                    {this.props.loading ? <h1>Cargando...</h1> : null}      
+                    <TableUser></TableUser>
+                        
+                    {/* {this.props.loading ? <h1>Cargando...</h1> : null}       */}
                                                           
                 </Paper>
             </div> 
@@ -61,21 +37,20 @@ handleClickGetUsers = () => {
     }    
 }
 
-// ListaUsers.propTypes = {
-//     red: PropTypes.array.isRequired
-// }
+
   
 
-const mapDispatchToProps = () => {
+const mapDispatchToProps = (users) => {
     return {
-        fetchUsers
+        users,
     };
 }
 
 
 const mapStateToProps = (state) => {
     return {
-        loading: state.loading
+        loading: state.loading,
+        fetchUsers,
     }
 }
 
--- src/components/Home/Users/TableUser.js
+++ src/components/Home/Users/TableUser.js
@@ -1,17 +1,37 @@
-if (this.props.users.users.length){
-    usersList = this.props.users.users.map(user => {
+import React from 'react';
+import {  Table, TableBody, TableCell, TableHead, TableRow, } from '@material-ui/core/';
+import { connect } from 'react-redux';
+
+const TableUser = (props) => 
+    <Table>
+        <TableHead>
+            <TableRow>
+                <TableCell>Nombre</TableCell>
+                <TableCell>Apellido Paterno</TableCell>
+                <TableCell>Apellido Materno</TableCell>
+                <TableCell>Edad</TableCell>                    
+            </TableRow>
+        </TableHead>
+        <TableBody>                      
+            { this.props.users.users.map(user => {
         return (
             <tr key ={user._id}>
                 <td>{user.nombre}</td>
                 <td>{user.apellidos.paterno}</td>
                 <td>{user.apellidos.materno}</td>
                 <td>{user.edad}</td>
-                <td style={flex}></td>
             </tr>
         )
-    })
-} else {
-    return (
-        messageError;
-    )
+            })
+
+        }
+        </TableBody>
+    </Table>  
+
+const mapStateToProps = (state) => {
+    return {
+        users: state.users
+    }
 }
+
+export default connect (mapStateToProps)(TableUser);
